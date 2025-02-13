1ece4d700338710e1ea06cd26def556a659aa5ea
gonca011 <155325095+gonca011@users.noreply.github.com>
2025-02-13 15:45:15 +0000

    Add files via upload

---
+++ Configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.dai2324.prototipodai.Configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") 
+                .allowedMethods("*") 
+                .allowedHeaders("*"); 
+    }
+}
