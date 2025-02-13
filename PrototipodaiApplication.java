f6e20798eb73a99ab2664a64581ec5359ef300b0
gonca011 <155325095+gonca011@users.noreply.github.com>
2025-02-13 15:54:36 +0000

    Add files via upload

---
+++ PrototipodaiApplication.java
@@ -0,0 +1,13 @@
+package com.dai2324.prototipodai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipodaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototipodaiApplication.class, args);
+	}
+
+}
